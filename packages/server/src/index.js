4bcca081588b01ae70e196e78f0779fd0c349ae4
Adrian Videnie <avidenie@gmail.com>
2019-09-02 22:00:20 +0300

    feat: support for Feed query

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  url: String!
+  description: String!
 }
 `
 
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+]
+
 const resolvers = {
   Query: {
-    info: _ => `This is the API of a HackerNews clone`
+    info: () => `This is the API of a HackerNews clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    url: parent => parent.url,
+    description: parent => parent.description
   }
 }
 const server = new GraphQLServer({
@@ -16,4 +37,4 @@ const server = new GraphQLServer({
   resolvers
 })
 
-server.start(_ => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`))
