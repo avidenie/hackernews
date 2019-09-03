c97563e426482dc9a7b924c9494b9dc6f3bddcc4
Adrian Videnie <avidenie@gmail.com>
2019-09-03 09:47:10 +0300

    feat: add database support

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,61 +1,50 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-const links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a HackerNews clone`,
-    feed: () => links,
     // eslint-disable-next-line no-unused-vars
-    link: (parent, args) => links.find(link => link.id === args.id)
+    feed: (root, args, context) => context.prisma.links(),
+    // eslint-disable-next-line no-unused-vars
+    link: (parent, args, context) => context.prisma.link({ id: args.id })
   },
   Mutation: {
     // eslint-disable-next-line no-unused-vars
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
         description: args.description
-      }
-      links.push(link)
-      return link
+      })
     },
     // eslint-disable-next-line no-unused-vars
-    update: (parent, args) => {
-      let existingLink = links.find(link => link.id === args.id)
-      if (existingLink) {
-        if (args.url) {
-          existingLink.url = args.url
-        }
-        if (args.description) {
-          existingLink.description = args.description
-        }
+    update: (root, args, context) => {
+      const newData = {}
+      if (args.url) {
+        newData.url = args.url
       }
-      return existingLink
+      if (args.description) {
+        newData.description = args.description
+      }
+
+      return context.prisma.updateLink({
+        where: { id: args.id },
+        data: newData
+      })
     },
     // eslint-disable-next-line no-unused-vars
-    delete: (parent, args) => {
-      let existingPosition = links.findIndex(link => link.id === args.id)
-      if (existingPosition === -1) {
-        return null
-      }
-      const deletedLink = links[existingPosition]
-      delete links[existingPosition]
-      return deletedLink
+    delete: (parent, args, context) => {
+      return context.prisma.deleteLink({ id: args.id })
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
