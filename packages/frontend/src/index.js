4d919ad2df16696b7f1ea2a624e5a8d30f61be67
Adrian Videnie <avidenie@gmail.com>
2019-09-06 13:05:30 +0300

    feat(frontend): authentication

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,10 +1,12 @@
 import { ApolloClient, InMemoryCache } from 'apollo-boost'
+import { setContext } from 'apollo-link-context'
 import { createHttpLink } from 'apollo-link-http'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
+import { AUTH_TOKEN } from './constants'
 import * as serviceWorker from './serviceWorker'
 import './styles/index.css'
 
@@ -12,8 +14,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
