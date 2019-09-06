f80b075d6f70bb9098752588f31c85d175cc40ab
Adrian Videnie <avidenie@gmail.com>
2019-09-06 19:54:23 +0300

    feat(frontend): subscribe to new links

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -9,6 +9,9 @@ import App from './components/App'
 import { AUTH_TOKEN } from './constants'
 import * as serviceWorker from './serviceWorker'
 import './styles/index.css'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -24,8 +27,27 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link,
   cache: new InMemoryCache()
 })
 
